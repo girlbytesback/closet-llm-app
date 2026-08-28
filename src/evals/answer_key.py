b7019140c4313a128ef1ee5b19c2f12f71b5f718
sophia <sophiasharifi@Mac.attwifi.manager>
2026-08-28 09:20:54 -0700

    prints and generates eval report

--- src/evals/answer_key.py
+++ src/evals/answer_key.py
@@ -1,24 +1,24 @@
 # measured using HEX color tool - serves as source of truth
 
 answer_key = {
-    "GARMENT_1.jpeg":  "#96446A",
-    "GARMENT_2.jpeg":  "#1B3671",
-    "GARMENT_3.jpeg":  "#1E1E20", #black oklou shirt
-    "GARMENT_4.jpeg":  "#573F3B",
-    "GARMENT_5.jpeg":  "#7D9681",
-    "GARMENT_6.jpeg":  "", #multi color
-    "GARMENT_7.jpeg":  "", #multi color
-    "GARMENT_8.jpeg":  "#91A193",
-    "GARMENT_9.jpeg":  "#544D2C",
-    "GARMENT_10.jpeg": "#979C78", #unique case, has multiple shades of green 
-    "GARMENT_11.jpeg": "", #multi color
-    "GARMENT_12.jpeg": "", #multi color
-    "GARMENT_13.jpeg": "#BBB97F",
-    "GARMENT_14.jpeg": "#D2DFA3",
-    "GARMENT_15.jpeg": "#273F3D",
-    "GARMENT_16.jpeg": "#9E3134",
-    "GARMENT_17.jpeg": "#9F3829",
-    "GARMENT_18.jpeg": "#A12625",
-    "GARMENT_19.jpeg": "#9B1A21",
-    "GARMENT_20.jpeg": "#751722"
+    "GARMENT_1.jpg":  "#96446A",
+    "GARMENT_2.jpg":  "#1B3671",
+    "GARMENT_3.jpg":  "#1E1E20", #black oklou shirt
+    "GARMENT_4.jpg":  "#573F3B",
+    "GARMENT_5.jpg":  "#7D9681",
+    "GARMENT_6.jpg":  "", #multi color
+    "GARMENT_7.jpg":  "", #multi color
+    "GARMENT_8.jpg":  "#91A193",
+    "GARMENT_9.jpg":  "#544D2C",
+    "GARMENT_10.jpg": "#979C78", #unique case, has multiple shades of green 
+    "GARMENT_11.jpg": "", #multi color
+    "GARMENT_12.jpg": "", #multi color
+    "GARMENT_13.jpg": "#BBB97F",
+    "GARMENT_14.jpg": "#D2DFA3",
+    "GARMENT_15.jpg": "#273F3D",
+    "GARMENT_16.jpg": "#9E3134",
+    "GARMENT_17.jpg": "#9F3829",
+    "GARMENT_18.jpg": "#A12625",
+    "GARMENT_19.jpg": "#9B1A21",
+    "GARMENT_20.jpg": "#751722"
 }
